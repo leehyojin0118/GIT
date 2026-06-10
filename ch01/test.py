c00b5e0b9381187bf4ae4c0924cd461ba1bed7b6
leehyojin <qw5854@gmail.com>
2026-06-10 14:49:30 +0900

    Add print statements for GitHub Web updates
    Added multiple print statements indicating updates.

--- ch01/test.py
+++ ch01/test.py
@@ -1,3 +1,7 @@
 print('Hello Git')
 print('Git Practice')
 # Auto login test
+
+print("This is updated from GitHub Web!")
+print("This is updated from GitHub Web!")
+print("This is updated from GitHub Web!")
